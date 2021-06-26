c7e488363b9df14cbf7323d22a69b883641e1e17
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-06-26 14:49:27 +0200

    Register routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //import express
 // const { response } = require("express"); - added in by ?eslint
-const express = require("express");
+const express = require('express');
 
 //store express function in variable
 const app = express();
@@ -8,12 +8,20 @@ const app = express();
 //register routes
 app.get('/',(request, response) =>{
     // console.log(response);
-    response.send("Messaged received");
+    response.send('Messaged received');
 });
 
-//listen for requests
+app.get('/node',(req,res)=>{
+    res.send({'title':'Well done, you found Node!'});
+});
+
+app.get('/codeyourfuture',(req,res)=>{
+    res.send('<h1 style="color:red; background-color:blue;">Your future is looking Bright!</h1>');
+});
+
+//listen for requests - typically placed at the end of the document after routes defined
 app.listen(3000, function() {
-    console.log("Server is listening");
+    console.log('Server is listening');
 });
 
 //handle requests
