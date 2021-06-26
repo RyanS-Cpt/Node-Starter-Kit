4ac80c3f14203980130951dbd0d61d2e435ed4ea
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-06-26 12:00:12 +0200

    Set up server and response

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+//import express
+const { response } = require("express");
 const express = require("express");
 
+//store express function in variable
 const app = express();
+
+//register routes
+app.get('/',(request, response) =>{
+    console.log(request);
+    response.send("Messaged received");
+});
+
+//listen for requests
+app.listen(3000, function() {
+    console.log("Server is listening");
+});
+
+//handle requests
 
